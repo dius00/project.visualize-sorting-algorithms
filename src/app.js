d2a5ac2ffc1dbaee01a402adeb71bf8194de1733
Antonio De Rinaldis <antonio.derinaldis@outlook.com>
2020-10-05 23:17:13 +0900

    integrated chartjs on the webpage

--- src/app.js
+++ src/app.js
@@ -4,23 +4,81 @@
 */
 
 // How can we use require here if it's frontend? We can thank webpack.
-const Sort = require("./Sort");
+const BubbleSort = require("./bubbleSort");
+const Chart = require("chart.js");
 
+//const Chart = require("Chart.js");
 // A link to our styles!
 require("./index.css");
 
-const sort = new Sort();
-sort.sort();
+const toBeOrdered = [10, 4, 6, 7, 1, 3, 9, 2, 5, 8];
+const bubble = new BubbleSort(toBeOrdered);
+//bubble.sort();
 
+let ctx = document.getElementById("myChart").getContext("2d");
+var myChart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"],
+    datasets: [
+      {
+        label: "#",
+        data: bubble.arr,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(54, 162, 235, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    aspectRatio	: 3,
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  },
+});
+//myChart.update();
+
+/*
 function createCheesyTitle(slogan) {
   const container = document.createElement("h1");
   const textNode = document.createTextNode(slogan);
   container.appendChild(textNode);
   return container;
-}
+}*/
 
-const title = createCheesyTitle(sort.returnValue("Re-Engineer Yourself"));
-document.getElementById("title").appendChild(title);
+//const title = createCheesyTitle(sort.returnValue("Re-Engineer Yourself"));
+//document.getElementById("title").appendChild(title);
 
 /*
     An simple example of how you can make your project a bit more
